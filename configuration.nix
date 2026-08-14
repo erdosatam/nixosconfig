d2677de911c293cb0e1b4e2c33a7445d68161c9d
Erdos <erdosatam@gmail.com>
2026-08-14 12:43:50 +0200

    lock

--- configuration.nix
+++ configuration.nix
@@ -92,20 +92,6 @@
     shell = pkgs.fish;
   };
 
-
-# home.nix
-  programs.swaylock = {
-   enable = true;
-   package = pkgs.swaylock-effects;
-   settings = {
-     screenshots = true;
-     clock = true;
-     indicator = true;
-     effect-blur = "7x5";
-     effect-vignette = "0.5:0.5";
-     fade-in = "0.2";
-   };
-  };
   services.upower.enable = true;
   # Rendszer szintű alap csomagok
   environment.systemPackages = with pkgs; [
