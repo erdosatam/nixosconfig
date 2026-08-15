54868d8d5c6f8c48d1814756e9af6283eafd9b5b
Erdos <erdosatam@gmail.com>
2026-08-15 16:17:04 +0200

    gpu

--- gaming.nix
+++ gaming.nix
@@ -13,9 +13,6 @@
 
   # Vulkan Validation Layers és Gaming eszközök
   environment.systemPackages = with pkgs; [
-    vulkan-validation-layers
-    vulkan-tools
-    vulkan-loader
     mangohud # Opcionális: FPS / hardware overlay
   ];
 }
--- hw_gpu.nix
+++ hw_gpu.nix
@@ -13,6 +13,14 @@
     enable32Bit = true;
     extraPackages = with pkgs; [
       libvdpau-va-gl
+      libva-vdpau-driver
+      mesa
+      vulkan-validation-layers
+      vulkan-extension-layer
+      vkd3d
+      vulkan-tools
+      vulkan-loader
+      dxvk
     ];
     extraPackages32 = with pkgs; [
       pkgsi686Linux.libvdpau-va-gl
