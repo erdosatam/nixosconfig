051d13988962428846b460a4889089dccd28bddd
Erdos <erdosatam@gmail.com>
2026-09-20 20:20:52 +0200

    change to nothing

--- desktop.nix
+++ desktop.nix
@@ -41,7 +41,7 @@
     defaultSession = "plasma";
   };
 
-  services.desktopManager.lxqt.enable = true;
+  services.desktopManager.plasma6.enable = true;
 
   services.xserver.xkb = {
     layout = "hu";
